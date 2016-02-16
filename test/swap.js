5a6977e4b1babe48f7bec065cad72e8877f5fdc7
khurrum qureshi <mkhurrumq@gmail.com>
2016-02-16 17:56:13 +0400

    modified quotesync signature and tests

--- test/swap.js
+++ test/swap.js
@@ -16,9 +16,21 @@ describe('swap', function () {
         }, Error, 'Provider should be one of the available provider.')
     });
 
+    it('it_throws_an_exception_when_options_is_not_an_object', function () {
+        assert.throw(function () {
+            swap.quoteSync('fds');
+        }, Error, 'Options should be an object')
+    });
+
+    it('it_throws_an_exception_when_options_does_not_contains_currency', function () {
+        assert.throw(function () {
+            swap.quoteSync({cache: true});
+        }, Error, 'Currency is required. Either pass it as string or object')
+    });
+
     it('it_throws_an_exception_when_provider_array_is_empty', function () {
         assert.throw(function () {
-            swap.quoteSync('USD/AED')
+            swap.quoteSync({currency: 'USD/AED'})
         }, Error, 'Add at least one provider to get the exchange rates')
     });
 
@@ -28,19 +40,19 @@ describe('swap', function () {
     });
 
     it('it_fetch_rate', function () {
-        var rate = swap.quoteSync('USD/AED');
+        var rate = swap.quoteSync({currency: 'USD/AED'});
         assert.isDefined(rate.getValue());
         assert.isDefined(rate.getDate());
     });
 
     it('it_fetch_rate_and_cache', function () {
-        var rate = swap.quoteSync('USD/AED', true);
+        var rate = swap.quoteSync({currency: 'USD/AED', cache:true});
         assert.isDefined(rate.getValue());
         assert.isDefined(rate.getDate());
     });
 
     it('it_fetch_rate_from_cache', function () {
-        var rate = swap.quoteSync('USD/AED', true);
+        var rate = swap.quoteSync({currency: 'USD/AED', cache:true});
         assert.isDefined(rate.getValue());
         assert.isDefined(rate.getDate());
     });
