5f32a87a736ccfbb471c7c8c6c128e610ca26d38
khurrum qureshi <mkhurrumq@gmail.com>
2016-02-17 17:28:42 +0400

    Added yahoo finance provider

--- test/provider/yahoofinance.js
+++ test/provider/yahoofinance.js
@@ -8,40 +8,40 @@
 
 var assert = require('chai').assert;
 var CurrencyPair = require('../../lib/model/currencypair');
-var GoogleFinanceProvider = require('../../lib/provider/googlefinance');
+var YahooFinanceProvider = require('../../lib/provider/yahoofinance');
 var UnsupportedCurrencyPairException = require('../../lib/error/UnsupportedCurrencyPairException');
 
-describe('google finance provider', function () {
+describe('yahoo finance provider', function () {
     it('it_throws_an_exception_when_rate_not_supported_sync', function () {
-        var provider = new GoogleFinanceProvider();
+        var provider = new YahooFinanceProvider();
         var currencyPair = new CurrencyPair('USD/XXL');
 
         assert.throw(function () {
             provider.fetchRateSync(currencyPair);
-        }, UnsupportedCurrencyPairException, 'The currency pair "USD/XXL" is not supported or "Google Finance" changed the response format')
+        }, UnsupportedCurrencyPairException, 'The currency pair "USD/XXL" is not supported or "Yahoo Finance" changed the response format')
     });
 
     it('it_fetches_a_rate_sync', function () {
-        var provider = new GoogleFinanceProvider();
+        var provider = new YahooFinanceProvider();
         var rate = provider.fetchRateSync(new CurrencyPair('USD/AED'));
         assert.isDefined(rate.getValue());
         assert.isDefined(rate.getDate());
     });
 
     it('it_throws_an_exception_when_rate_not_supported', function (done) {
-        var provider = new GoogleFinanceProvider();
+        var provider = new YahooFinanceProvider();
         var currencyPair = new CurrencyPair('USD/XXL');
 
         provider.fetchRate(currencyPair, function (err, rate) {
             assert.throw(function () {
                 throw err;
-            }, UnsupportedCurrencyPairException, 'The currency pair "USD/XXL" is not supported or "Google Finance" changed the response format');
+            }, UnsupportedCurrencyPairException, 'The currency pair "USD/XXL" is not supported or "Yahoo Finance" changed the response format');
             done();
         });
     });
 
     it('it_fetches_a_rate', function (done) {
-        var provider = new GoogleFinanceProvider();
+        var provider = new YahooFinanceProvider();
         provider.fetchRate(new CurrencyPair('USD/AED'), function (err, rate) {
             assert.isDefined(rate.getValue());
             assert.isDefined(rate.getDate());
